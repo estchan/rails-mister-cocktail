aa6adb9b3c4df1299411ecf852a36f1f305dad29
Esther Chan <est.chanwh@gmail.com>
2019-02-21 11:55:39 +0000

    Prepare rails app with external specs

--- db/schema.rb
+++ db/schema.rb
@@ -21,22 +21,10 @@ ActiveRecord::Schema.define(version: 2019_02_21_114215) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "doses", force: :cascade do |t|
-    t.text "description"
-    t.bigint "cocktail_id"
-    t.bigint "dose_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
-    t.index ["dose_id"], name: "index_doses_on_dose_id"
-  end
-
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "doses", "cocktails"
-  add_foreign_key "doses", "doses"
 end
